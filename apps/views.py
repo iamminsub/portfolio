e8e3afc212344cb552aeea4d1de558b12c654165
AHN <iamminsub@naver.com>
2014-07-21 22:35:56 +0900

    deploy하기위해 코드변경/

--- apps/views.py
+++ apps/views.py
@@ -1,13 +1,13 @@
 #-*- coding: utf-8 -*-
 from flask import render_template, Flask
-#from apps import app #deploy할때 주석해 
+from apps import app #deploy할때 주석해 
 
-app = Flask(__name__) #deploy할때 주석 
+#app = Flask(__name__) #deploy할때 주석 
 @app.route('/')
 @app.route('/blog')
 def blog():
     return render_template("blog.html")
 
 
-if __name__ == "__main__": #deploy할때 주석
+#if __name__ == "__main__": #deploy할때 주석
 	app.run()
