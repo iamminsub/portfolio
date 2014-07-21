7acc038181736360cbcb8bfdb4050efe6773cbe9
AHN <iamminsub@naver.com>
2014-07-21 22:31:41 +0900

    flask 추가!!

---
+++ apps/views.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+from flask import render_template, Flask
+#from apps import app #deploy할때 주석해 
+
+app = Flask(__name__) #deploy할때 주석 
+@app.route('/')
+@app.route('/blog')
+def blog():
+    return render_template("blog.html")
+
+
+if __name__ == "__main__": #deploy할때 주석
+	app.run()
